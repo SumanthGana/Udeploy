b01b0ad8f6f47f527a31a5aa3c0470388dd44ec3
SumanthGana <67053142+SumanthGana@users.noreply.github.com>
2022-03-28 14:37:22 +0530

    Update Calculator.java

--- src/main/java/Add/Calculator.java
+++ src/main/java/Add/Calculator.java
@@ -14,12 +14,12 @@ public class Calculator {
 	   }
 	 
 	   // Integer divide. Return a truncated int.
-	   public static int divInt(int number1, int number2) {
-	      if (number2 == 0) {
-	         throw new IllegalArgumentException("Cannot divide by 0!");
-	      }
-	      return number1 / number2;
-	   }
+	  // public static int divInt(int number1, int number2) {
+	   //   if (number2 == 0) {
+	     //    throw new IllegalArgumentException("Cannot divide by 0!");
+	     // }
+	     // return number1 / number2;
+	  // }
 	 
 	   // Real number divide. Return a double.
 	   public static double divReal(int number1, int number2) {
